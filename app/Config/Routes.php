0dcd2dff21c43b8bc930cd6fffbcc8e88f0539e1
gmarxdev <gmarbandoy@gmail.com>
2025-08-27 23:58:18 +0800

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,10 +7,14 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-$routes->group('api', function($routes) {
-    $routes->get('users', 'Api\UserController::index');
-    $routes->get('users/(:num)', 'Api\UserController::show/$1');
-    $routes->post('users', 'Api\UserController::create');
-    $routes->put('users/(:num)', 'Api\UserController::update/$1');
-    $routes->delete('users/(:num)', 'Api\UserController::delete/$1');
+// $routes->group('api', function($routes) {
+//     $routes->get('users', 'Api\UserController::index');
+//     $routes->get('users/(:num)', 'Api\UserController::show/$1');
+//     $routes->post('users', 'Api\UserController::create');
+//     $routes->put('users/(:num)', 'Api\UserController::update/$1');
+//     $routes->delete('users/(:num)', 'Api\UserController::delete/$1');
+// });
+
+$routes->group('api', ['namespace' => 'App\Controllers\Api'], function($routes) {
+    $routes->resource('users');
 });
