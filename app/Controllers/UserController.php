468c774286239f0173f909853bdf644069686cb3
gmarxdev <gmarbandoy@gmail.com>
2025-08-27 23:42:02 +0800

    update

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class ApiUserController extends BaseController
+class UserController extends BaseController
 {
     public function index()
     {
