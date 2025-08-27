ec386a547ae5179b54b515217d4aa46d11612cff
gmarxdev <gmarbandoy@gmail.com>
2025-08-27 23:38:23 +0800

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('api', function($routes) {
+    $routes->get('users', 'Api\Users::index');
+    $routes->get('users/(:num)', 'Api\Users::show/$1');
+    $routes->post('users', 'Api\Users::create');
+    $routes->put('users/(:num)', 'Api\Users::update/$1');
+    $routes->delete('users/(:num)', 'Api\Users::delete/$1');
+});
